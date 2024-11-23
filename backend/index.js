c6d24f74154f1101783cc119daf7c30e8ea9fbc5
issmathe <issmathe@gmail.com>
2024-11-23 12:38:38 +0100

    günded

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const database = require('./database.js');
+const denemeRoute=require("./routes/denemes.js") 
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 const PORT = process.env.PORT ||  5000;
-
+app.use("/api",denemeRoute)
 
 
 app.listen(PORT, () => {
