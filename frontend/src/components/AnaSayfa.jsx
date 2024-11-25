154d88fd3327ce6e1d84e7c6d424b8af27046b01
issmathe <issmathe@gmail.com>
2024-11-25 22:49:06 +0100

    sadf

---
+++ frontend/src/components/AnaSayfa.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const AnaSayfa = () => {
+  return (
+    <div style={styles.container}>
+      <header style={styles.header}>
+        <h1 style={styles.title}>Almanca Hazinem</h1>
+      </header>
+      <div style={styles.menu}>
+        <button style={styles.button}>Anasayfa</button>
+        <button style={styles.button}>Kelimeler</button>
+        <button style={styles.button}>Tekrar</button>
+        <button style={styles.button}>Oyun</button>
+        <button style={styles.button}>Bize Ulaşın</button>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f0f0f0",
+    padding: "10px",
+  },
+  header: {
+    marginBottom: "20px",
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "24px",
+    fontWeight: "bold",
+    color: "#333",
+  },
+  menu: {
+    width: "100%",
+    maxWidth: "400px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+  },
+  button: {
+    padding: "15px",
+    backgroundColor: "#4CAF50",
+    color: "#fff",
+    fontSize: "16px",
+    border: "none",
+    borderRadius: "5px",
+    textAlign: "center",
+    cursor: "pointer",
+  },
+};
+
+export default AnaSayfa;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Header from '../components/header/Header.jsx'
-import Deneme from '../components/Deneme.jsx'
-import DenemeGoster from '../components/DenemeGoster.jsx'
+import Sayfa from '../components/AnaSayfa.jsx'
 
 const Home = () => {
   return (
     <div>
-        <Header/>
-        <Deneme/>
-        <DenemeGoster/>
+              <Sayfa/>
+
+
     </div>
   )
 }
