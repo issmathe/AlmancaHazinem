f75e3c0ac88839ac718fa16447f1754c53175fac
issmathe <issmathe@gmail.com>
2024-11-26 22:09:56 +0100

    dsfa

--- frontend/src/pages/kelimelerSayfa/DerArtikelPage.jsx
+++ frontend/src/pages/kelimelerSayfa/DerArtikelPage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import DerArtikel from '../../components/kelimler/DerArtikel'
 import DerArtikelGoster from '../../components/kelimler/DerArtikelGoster'
 
 const DerArtikelPage = () => {
   return (
     <div>
-      <DerArtikel/>
+
       <DerArtikelGoster/>
     </div>
   )
